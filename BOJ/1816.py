7c9858eb18e56ffb5bc60a3847c2c6eb811cd6e2
soojeongmin <soojeongmin@soongsil.ac.kr>
2024-11-11 18:04:51 +0900

    Modify

--- BOJ/1816.py
+++ BOJ/1816.py
@@ -3,7 +3,7 @@ for _ in range(c):
     n = int(input())
     for i in range(2, 1_000_001):
         if n%i == 0:
-            print("NO")
+            print('NO')
             break
         if i == 1_000_000:
-            print("YES")
+            print('YES')
