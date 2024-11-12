8cf46f44cd65b1115570e38692873f86f8ca6526
Soojeong <75717432+soojeongmin@users.noreply.github.com>
2024-11-12 23:24:00 +0900

    Create 11653.py

---
+++ BOJ/11653.py
@@ -0,0 +1,13 @@
+k = int(input())
+arr = []
+d = 2
+
+while d * d <= k:
+    while k % d == 0:
+        arr.append(d)
+        k //= d
+    d += 1
+if k > 1: arr.append(k)
+
+for i in arr:
+    print(i)
